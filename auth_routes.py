4a0651398e45961ca9d17c30e280a4f4cab6d387
alexandre <alexandre020602@gmail.com>
2026-02-10 13:10:33 -0300

    criando minhas duas rotas e passando o prefixo + tags

---
+++ auth_routes.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+auth_route = APIRouter(prefix="/auth", tags=["auth"])
---
+++ order_routes.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+auth_route = APIRouter(prefix="/orders", tags=["orders"])
