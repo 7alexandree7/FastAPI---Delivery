f33bb367fb8a1804cb55a634662560b39ab8a066
alexandre <alexandre020602@gmail.com>
2026-02-10 16:26:21 -0300

    creating two auth and order routes and testing

--- auth_routes.py
+++ auth_routes.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
 auth_route = APIRouter(prefix="/auth", tags=["auth"])
+
+@auth_route.get("/")
+async def get_auth():
+    return {"message": "voce acessou a rota de auth"}
--- order_routes.py
+++ order_routes.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
-auth_route = APIRouter(prefix="/orders", tags=["orders"])
+order_route = APIRouter(prefix="/orders", tags=["orders"])
+
+
+@order_route.get("/")
+async def get_orders():
+    return {"messagem": "voce acessou a rota de orders"}
