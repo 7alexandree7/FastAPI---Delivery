f00a932ba75d850b5cee71c98c89ba9b68a0b6e3
alexandre <alexandre020602@gmail.com>
2026-02-13 12:55:04 -0300

    add route create_order

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -3,8 +3,10 @@ from db.session import get_session
 from schema.schemas import OrderSchema
 from sqlalchemy.orm import Session
 from models.models import Order
+from security.verify_token import verify_token
+from models.models import User
 
-order_route = APIRouter(prefix="/orders", tags=["orders"])
+order_route = APIRouter(prefix="/orders", tags=["orders"], dependencies=[Depends(verify_token)])
 
 @order_route.get("/")
 async def get_orders():
@@ -13,7 +15,35 @@ async def get_orders():
 
 @order_route.post("/order")
 async def create_order(orderSchema: OrderSchema, session: Session = Depends(get_session)):
-    new_order = Order(user=orderSchema.user)
-    session.add(new_order)
+    try:
+        new_order = Order(user=orderSchema.user)
+        session.add(new_order)
+        session.commit()
+        session.refresh(new_order)
+
+        return {"message": f"order created {new_order.id}"}
+    
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail="error creating order")
+
+
+@order_route.post("/order/cancel/{order_id}")
+async def cancel_order(order_id: int, user: User = Depends(verify_token), session: Session = Depends(get_session)):
+
+    order = session.query(Order).filter(Order.id == order_id).first()
+
+    if not order:
+        raise HTTPException(status_code=404, detail="order not found")
+    
+    if not (user.admin and order.user != user.id):
+        raise HTTPException(status_code=403, detail="you dont have permission to cancel this order")
+    
+    order.status = "cancelado"
     session.commit()
-    return HTTPException(status_code=201, detail=f"order created {new_order.id}")
+
+  
+    return {
+        "message": f"order {order.id} canceled",
+        "order": order
+    }
